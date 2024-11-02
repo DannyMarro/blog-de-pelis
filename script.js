c342c872233c15424d5ade50a128ae2a3a0d4228
DannyMarro <Danielalejandro.cruz.marro@gmail.com>
2024-11-01 19:46:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-function openModal(url) {
-    document.getElementById("trailer").src = url;
-    document.getElementById("modal").style.display = "block";
+function openTrailer(url) {
+    window.open(url, '_blank');
 }
 
-function closeModal() {
-    document.getElementById("modal").style.display = "none";
-    document.getElementById("trailer").src = ""; // Detener el video al cerrar
-}
-
-// Cerrar modal al hacer clic fuera de él
-window.onclick = function(event) {
-    const modal = document.getElementById("modal");
-    if (event.target == modal) {
-        closeModal();
-    }
-};
